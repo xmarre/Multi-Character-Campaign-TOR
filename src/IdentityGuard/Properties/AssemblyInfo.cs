7051eebe2fed8994732f2a867368d4990636d2d7
xmarre <54859656+xmarre@users.noreply.github.com>
2026-08-13 09:44:27 +0200

    Bump IdentityGuard assembly version to 1.3.2

--- src/IdentityGuard/Properties/AssemblyInfo.cs
+++ src/IdentityGuard/Properties/AssemblyInfo.cs
@@ -5,5 +5,5 @@
 using System.Reflection;
 
 [assembly: AssemblyTitle("MultiCharacterCampaignTOR.IdentityGuard")]
-[assembly: AssemblyFileVersion("1.3.1.0")]
-[assembly: AssemblyVersion("1.3.1.0")]
+[assembly: AssemblyFileVersion("1.3.2.0")]
+[assembly: AssemblyVersion("1.3.2.0")]
