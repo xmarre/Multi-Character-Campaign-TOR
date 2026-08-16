c6181ef3b9a1e9258a215268ff94cbb5fd53100a
xmarre <54859656+xmarre@users.noreply.github.com>
2026-08-16 19:33:43 +0200

    Bump IdentityGuard to v1.3.4

--- src/IdentityGuard/Properties/AssemblyInfo.cs
+++ src/IdentityGuard/Properties/AssemblyInfo.cs
@@ -5,5 +5,5 @@
 using System.Reflection;
 
 [assembly: AssemblyTitle("MultiCharacterCampaignTOR.IdentityGuard")]
-[assembly: AssemblyFileVersion("1.3.3.0")]
-[assembly: AssemblyVersion("1.3.3.0")]
+[assembly: AssemblyFileVersion("1.3.4.0")]
+[assembly: AssemblyVersion("1.3.4.0")]
